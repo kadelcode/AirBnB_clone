d2a2ff2c4a7e8cd31946d24dc88a54712893ac1f
Ayodele Kadiri <kadelcode@gmail.com>
2023-03-07 15:44:26 -0800

    Update code

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/urs/bin/python3
 """ A BaseModel class """
+import models
 from uuid import uuid4
 from datetime import datatime
 
@@ -18,3 +19,12 @@ class BaseModel:
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         tform = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
