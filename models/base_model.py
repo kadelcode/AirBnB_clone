942e82366fb5bbb3020c0d64598d125a5f1d97d1
Ayodele Kadiri <kadelcode@gmail.com>
2023-03-07 15:38:36 -0800

    update basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -2,3 +2,19 @@
 """ A BaseModel class """
 from uuid import uuid4
 from datetime import datatime
+
+
+class BaseModel:
+    """ Defines all common attributes/methods for other classes """
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize a new BaseModel
+
+        Args:
+            *args (any): Unused
+            **kwargs (dict): Key/value pairs of attributes
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
