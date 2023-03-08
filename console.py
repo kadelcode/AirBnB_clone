3f2a79028d372e77a49b5cf55aeb14153ed670db
Ayodele Kadiri <kadelcode@gmail.com>
2023-03-07 21:46:27 -0800

    The console- Update code

--- console.py
+++ console.py
@@ -3,17 +3,36 @@
 import cmd
 
 
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+
 class HBNBCommand(cmd.Cmd):
     """ A command line interpreter """
     prompt = ' (hbnb) '
 
-    def do_quit(self,arg):
+    def do_quit(self, arg):
         'Quit command to exit the program'
         return True
 
     def do_EOF(self, arg):
         'Exits the program'
         return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
