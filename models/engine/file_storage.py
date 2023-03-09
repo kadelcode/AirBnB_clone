b38cace7d12906f24ced80ffccdb9bee4b413ddc
Ayodele Kadiri <kadelcode@gmail.com>
2023-03-09 14:57:48 -0800

    update file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 """The FileStorage class"""
 import json
 from models.base_model import BaseModel
-
+from models.user import User
 class FileStorage:
     """ Represent an abstracted storage engine
 
