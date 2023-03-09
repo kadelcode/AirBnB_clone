c7b859c4a4ac134999c2841402a898f894f0f3b8
Ayodele Kadiri <kadelcode@gmail.com>
2023-03-09 15:26:29 -0800

    state class

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A State class that inherits from BaseModel """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Defines a State class
+
+        Public class attribute:
+            name (str): The name of the State.
+    """
+    name = ""
